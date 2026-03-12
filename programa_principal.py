9831480a5a37cf75d374e9b8f71e380da0b4dd38
anapiuma-ship-it <mpiumaisaurralde@udesa.edu.ar>
2026-03-12 18:39:17 -0300

    Update programa_principal.py

--- programa_principal.py
+++ programa_principal.py
@@ -1,12 +1,8 @@
 import funciones_habitos
 
 lista = funciones_habitos.registrar_habitos()
+resultado = funciones_habitos.analizar_habitos(lista)
 
-resultado = funciones_habitos.estadistica_actividades_realizadas(lista)
+print("Resumen de actividades:")
+print(resultado)
 
-print("Resumen de actividades: ")
-
-
-for actividad, cantidad in resultado.items():
-    
-    print(actividad, ":", cantidad)
