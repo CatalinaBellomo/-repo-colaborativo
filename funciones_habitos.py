e21640471959d8da27e875f9ac8c000a5cb52400
allegragegenschatz <agegenschatz@udesa.edu.ar>
2026-03-12 15:44:37 -0300

    Update funciones_habitos.py

--- funciones_habitos.py
+++ funciones_habitos.py
@@ -1,1 +1,26 @@
 
+def esatadistica_actividades_realizadas(actividad):
+    """
+
+    Parameters
+    ----------
+    actividad : str
+        La actividad realizada por el participante.
+
+    Returns 
+    -------
+    frecuencia : dicc
+        diccionario cuyos claves son las activcidades y los valores son la frecuencia en la que aparecen.
+
+    """
+    actividad = input('Ingrese la actividad realizada')
+    lista = []
+    lista.append(actividad)
+    frecuencia = {}
+    for act in lista:
+        if act in frecuencia:
+            frecuencia[act] +=1
+        else:
+            frecuencia[act] = 1
+    return frecuencia
+
