398d67f80969550275d2f7ad6d957ad65586ccae
CatalinaBellomo <catalinabellomo05@gmail.com>
2026-03-13 00:45:54 -0300

    Update funciones:habitos.py

--- funciones:habitos.py
+++ funciones:habitos.py
@@ -6,27 +6,65 @@ Created on Thu Mar 12 15:19:40 2026
 @author: allegra
 """
 
-def esatadistica_actividades_realizadas(actividad):
-    """
 
-    Parameters
+#Funcion 1 
+def registrar_habitos():
+    '''
+    Permite al usuario ingresar actividades realizadas hasta escribir 'salir' para terminar el programa
+    
+   
+    Returns
+    -------
+    actividades : list
+        Una lista con las actividades ingresadas por el usuario.
+
+    '''
+    
+    
+    actividades = []
+
+    while True:
+        actividad = input("Ingrese la actividad realizada, escriba salir cuando quiera terminar: ")
+
+        if actividad == "salir":
+            break
+
+        actividades.append(actividad)
+
+    return actividades
+        
+# probar la funcion 
+
+print(registrar_habitos())
+
+#Funcion 2 
+def analizar_habitos(actividades): 
+    '''
+    
+
+   Recibe uan lista con todas las actividades realziadas por el usario y cuenta cuanta veces se realiza cada actividad
     ----------
-    actividad : str
-        La actividad realizada por el .
+    actividades : list
+        lista con todas las actividades realizadas 
 
     Returns
     -------
-    frecuencia : TYPE
-        DESCRIPTION.
+    diccionario : dict
+        diccionario donde la clave es cada actividad realizada y el valor es la canitdad de veces que realizo el usuario
 
-    """
-    actividad = input('Ingrese la actividad realizada')
-    lista = []
-    lista.append(actividad)
-    frecuencia = {}
-    for act in lista:
-        if act in frecuencia:
-            frecuencia[act] +=1
+    '''
+    diccionario={}
+    for actividad in actividades:
+        if actividad in diccionario:
+            diccionario[actividad] += 1
         else:
-            frecuencia[act] = 1
-    return frecuencia
+            diccionario[actividad] = 1
+
+    return diccionario
+
+#probar la funcion
+acciones=['comer','comer','saltar','cantar','correr','dormir','correr']
+print(analizar_habitos(acciones))
+
+
+
