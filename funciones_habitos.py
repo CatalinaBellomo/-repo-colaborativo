d218f8674097529162bda08c8989be1405001bb0
anapiuma-ship-it <mpiumaisaurralde@udesa.edu.ar>
2026-03-31 12:49:52 -0300

    no se

--- funciones_habitos.py
+++ funciones_habitos.py
@@ -1,26 +1,68 @@
 
-def esatadistica_actividades_realizadas(actividad):
-    """
-
-    Parameters
-    ----------
-    actividad : str
-        La actividad realizada por el participante.
-
+def registrar_habitos(): 
+    """ 
+    Pide al usuario que ingrese la cantidad de actividades que desee y devuelve
+    una lista con todas ellas. 
+    
     Returns 
-    -------
-    frecuencia : dicc
-        diccionario cuyos claves son las activcidades y los valores son la frecuencia en la que aparecen.
+    ------- 
+    lista : list -- lista de actividades ingresada por el usuario 
+    """ 
+    actividades = input('Ingrese actividades: ') 
+    lista = actividades.split() 
+    return lista 
 
-    """
-    actividad = input('Ingrese la actividad realizada')
-    lista = []
-    lista.append(actividad)
-    frecuencia = {}
-    for act in lista:
-        if act in frecuencia:
-            frecuencia[act] +=1
-        else:
-            frecuencia[act] = 1
-    return frecuencia
+def analizar_habitos(lista): 
+    """ 
+    Toma la lista de la funcion anterior (registrar_habitos()) y crea un
+    diccionario que indica cuantas veces se repite cada actividad 
+    Parameters 
+    ---------- 
+    lista : list -- lista de actividades (previamente creade por la funcion 
+                                          anterior) 
+    Returns 
+    ------- 
+    frecuencia : dict -- diccionario con la frecuencia de cada actividad, 
+    cuantas veces se repite cada una en la lista 
+    """ 
+    frecuencia = {} 
+    for act in lista: 
+        if act in frecuencia: 
+            frecuencia[act] = frecuencia[act] + 1 
+        else: frecuencia[act] = 1 
+    return frecuencia 
+
+'''
+lista = registrar_habitos() 
+resultado = analizar_habitos(lista) 
+print(resultado)
+'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
--- programa_principal.py
+++ programa_principal.py
@@ -1,12 +1,7 @@
 import funciones_habitos
 
 lista = funciones_habitos.registrar_habitos()
+resultado = funciones_habitos.analizar_habitos(lista)
 
-resultado = funciones_habitos.estadistica_actividades_realizadas(lista)
-
-print("Resumen de actividades: ")
-
-
-for actividad, cantidad in resultado.items():
-    
-    print(actividad, ":", cantidad)
+print("Resumen de actividades:")
+print(resultado)
